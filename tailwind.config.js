9d5e184165fe809967a59caf6483abb7969eba6e
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-16 00:04:45 +0100

    Styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['*.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    colors: {
+      primary: '#943A0A',
+      dark: '#363636',
+      white: '#fff',
+      lightGrey: '#FAFAFA',
+      darkGrey: '#EBEBEB',
+      accent: '#004745',
+    },
+    fontFamily: {
+      lust: ['Lust Script', 'sans-serif'],
+      ofelia: ['Ofelia Display', 'sans-serif'],
+    },
+    fontWeight: {
+      extralight: 200,
+      light: 300,
+      regular: 400,
+      bold: 700,
+    },
   },
   plugins: [],
 };
