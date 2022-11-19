071699cf1d47d27a71d7f8ab98f5920198850ffb
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-19 13:52:44 +0100

    added auth functionality

---
+++ src/js/api/auth/login.js
@@ -0,0 +1,30 @@
+import { API_AUCTION_URL } from '../constants.js';
+import * as storage from '../../handlers/storage/index.js';
+
+const action = '/auth/login';
+const method = 'POST';
+
+export async function login(profile) {
+  const loginURL = API_AUCTION_URL + action;
+  const body = JSON.stringify(profile);
+  const options = {
+    method,
+    body,
+    headers: {
+      'Content-Type': 'application/json;',
+    },
+  };
+
+  try {
+    const response = await fetch(loginURL, options);
+    const { accessToken, ...profile } = await response.json();
+
+    switch (response.status) {
+      case 200:
+        storage.save('token', accessToken);
+        storage.save('profile', profile);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
---
+++ src/js/api/auth/register.js
@@ -0,0 +1,28 @@
+import { API_AUCTION_URL } from '../constants.js';
+
+const action = '/auth/register';
+const method = 'POST';
+
+export async function register(profile) {
+  const registerURL = API_AUCTION_URL + action;
+  const body = JSON.stringify(profile);
+  const options = {
+    method,
+    body,
+    headers: {
+      'Content-Type': 'application/json;',
+    },
+  };
+
+  try {
+    const response = await fetch(registerURL, options);
+    const result = await response.json();
+
+    switch (response.status) {
+      case 201:
+        return result;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
