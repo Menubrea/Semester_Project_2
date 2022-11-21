03b0b4b3d21f222fe32763ce625c36e35db146af
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-21 22:46:14 +0100

    updated index file

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
-import { getListings } from './api/listings/read.js';
+import { setGetListing } from './handlers/getListing.js';
 import { setGetListings } from './handlers/getListings.js';
+import { setLoginFormListener } from './handlers/login.js';
+import { setRegisterFormListener } from './handlers/register.js';
+import { profileData } from './templates/profileData.js';
 
-setGetListings();
-getListings();
+const path = location.pathname;
+
+if (path === '/' || path === '/index.html') {
+  setGetListings();
+  profileData();
+} else if (path === '/listing/') {
+  setGetListing();
+  profileData();
+} else if (path === '/auth/register/') {
+  setRegisterFormListener();
+} else if (path === '/auth/login/') {
+  setLoginFormListener();
+}
+
+console.log(location.pathname);
