da8e052f51900b169b0a2baeedcb9e905dba43be
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-26 14:06:36 +0100

    updated barrelfile

--- src/js/handlers/index.js
+++ src/js/handlers/index.js
@@ -2,3 +2,5 @@ export * from './getListings.js';
 export * from './getListing.js';
 export * from './login.js';
 export * from './register.js';
+export * from './makeBid.js';
+export * from './logout.js';
