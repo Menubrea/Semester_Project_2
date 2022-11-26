eb4cb483edb392eb4ccd3ec6cd16367ab55f193c
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-26 14:06:49 +0100

    updated routing

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
+import { setCreateListings } from './handlers/createListing.js';
 import { setGetListing } from './handlers/getListing.js';
 import { setGetListings } from './handlers/getListings.js';
 import { setLoginFormListener } from './handlers/login.js';
 import { setRegisterFormListener } from './handlers/register.js';
+import { createPreviewTemplate } from './templates/preview.js';
 import { profileData } from './templates/profileData.js';
 
 const path = location.pathname;
@@ -9,7 +11,11 @@ const path = location.pathname;
 if (path === '/' || path === '/index.html') {
   setGetListings();
   profileData();
+  createPreviewTemplate();
+  setCreateListings();
 } else if (path === '/listing/') {
+  createPreviewTemplate();
+  setCreateListings();
   setGetListing();
   profileData();
 } else if (path === '/auth/register/') {
