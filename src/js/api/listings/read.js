5c6e11d3ffa9ee5583f544d5b25c6ab993b92431
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-12-15 23:13:18 +0100

    improved ux and error message

--- src/js/api/listings/read.js
+++ src/js/api/listings/read.js
@@ -1,17 +1,30 @@
+import { displayErrorMessage } from '../../components/errorMessage.js';
+import { clearLoader } from '../../components/loader.js';
 import { API_AUCTION_URL } from '../constants.js';
 
 const action = '/listings/';
 const details =
   '?_seller=true&_bids=true&_active=true&sort=created&sortOrder=desc';
+const container = document.querySelector('#forAllListings');
 
 export async function getListings() {
   const listingsURL = `${API_AUCTION_URL}${action}${details}`;
-  const response = await fetch(listingsURL);
+  try {
+    const response = await fetch(listingsURL);
 
-  if (response.ok) {
-    const listings = await response.json();
-    return listings;
-  } else {
-    throw new Error();
+    switch (response.status) {
+      case 200: {
+        const listings = await response.json();
+        window.onload = setTimeout(() => {
+          clearLoader();
+        }, 200);
+        return listings;
+      }
+      default:
+        throw new Error();
+    }
+  } catch (err) {
+    displayErrorMessage();
+    console.log(err);
   }
 }
